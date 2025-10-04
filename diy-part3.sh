205bfaed2279dc1be5894ada944d1b791e030a99
jlzsss <jlz19821031@gmail.com>
2025-10-05 07:58:52 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -34,7 +34,9 @@ rm -rf package/feeds/luci2/luci-app-turboacc
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+rm -rf feeds/luci/applications/luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
 
 rm -rf package/feeds/lede/php7
 # rm -rf package/feeds/packages/php7
