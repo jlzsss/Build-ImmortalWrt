b1c8d4e4afafb0c4e187a492936da92520e3cf83
jlzsss <jlz19821031@gmail.com>
2025-10-05 07:17:28 +0800

    修改依赖关系

--- diy-part3.sh
+++ diy-part3.sh
@@ -31,6 +31,10 @@ rm -rf package/feeds/luci2/luci-app-turboacc
 ./scripts/feeds install -p packages2 luci-app-quickstart
 ./scripts/feeds install -p luci2 transmission
 ./scripts/feeds install -p luci2 transmission-web-control
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 
 rm -rf package/feeds/lede/php7
 # rm -rf package/feeds/packages/php7
